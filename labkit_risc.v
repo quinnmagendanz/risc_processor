c1ed6dc7679e84871e9514f46e0d93954e84e5e6
Quinn Magendanz <qpm3@mit.edu>
2018-11-18 17:03:11 -0500

    Add Nexus files.

---
+++ labkit_risc.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+module labkit(
+   input CLK100MHZ,
+   input[15:0] SW, 
+   input BTNC, BTNU, BTNL, BTNR, BTND,
+   output[3:0] VGA_R, 
+   output[3:0] VGA_B, 
+   output[3:0] VGA_G,
+   output[7:0] JA, 
+   output VGA_HS, 
+   output VGA_VS, 
+   output LED16_B, LED16_G, LED16_R,
+   output LED17_B, LED17_G, LED17_R,
+   output[15:0] LED,
+   output[7:0] SEG,  // segments A-G (0-6), DP (7)
+   output[7:0] AN    // Display 0-7
+   );
+   
+
+// create 25mhz system clock
+    wire clock_25mhz;
+    clock_quarter_divider clockgen(.clk100_mhz(CLK100MHZ), .clock_25mhz(clock_25mhz));
+
+//  instantiate 7-segment display;  
+    wire [31:0] data;
+    wire [6:0] segments;
+    display_8hex display(.clk(clock_25mhz),.data(data), .seg(segments), .strobe(AN));    
+    assign SEG[6:0] = segments;
+    assign SEG[7] = 1'b1;
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+//  remove these lines and insert your lab here
+
+    assign LED = SW;     
+    assign JA[7:0] = 8'b0;
+    assign data = {28'h0123456, SW[3:0]};   // display 0123456 + SW
+    assign LED16_R = BTNL;                  // left button -> red led
+    assign LED16_G = BTNC;                  // center button -> green led
+    assign LED16_B = BTNR;                  // right button -> blue led
+    assign LED17_R = BTNL;
+    assign LED17_G = BTNC;
+    assign LED17_B = BTNR; 
+
+
+
+//
+//////////////////////////////////////////////////////////////////////////////////
+endmodule
+
+module clock_quarter_divider(input clk100_mhz, output reg clock_25mhz = 0);
+    reg counter = 0;
+
+    // VERY BAD VERILOG
+    // VERY BAD VERILOG
+    // VERY BAD VERILOG
+    // But it's a quick and dirty way to create a 25Mhz clock
+    // Please use the IP Clock Wizard under FPGA Features/Clocking
+    //
+    // For 1 Hz pulse, it's okay to use a counter to create the pulse as in
+    // assign onehz = (counter == 100_000_000); 
+    // be sure to have the right number of bits.
+
+    always @(posedge clk100_mhz) begin
+        counter <= counter + 1;
+        if (counter == 0) begin
+            clock_25mhz <= ~clock_25mhz;
+        end
+    end
+endmodule
