e3b6cb87d1d634dc1318a1b0ec74a9d5f0590417
Bradley D Jomard <bjomard@mit.edu>
2018-12-06 14:24:26 -0500

    fixed regfile

--- regfile.v
+++ regfile.v
@@ -4,15 +4,15 @@ module regfile (
         input [31:0] wdata,
 		input [4:0] ra, rb, rc,
 		input ra2sel, wasel, werf, clock,
-		output reg [31:0] radata, rbdata
+		output [31:0] radata, rbdata
 		);
 
 	reg [31:0] data [31:0];
 	integer i = 0;
-	reg [4:0] ra1 = 0;
-	reg [4:0] ra2 = 0;
-	reg [4:0] wa = 0;
-	reg we = 0;
+	wire [4:0] ra1 = 0;
+	wire [4:0] ra2 = 0;
+	wire [4:0] wa = 0;
+	wire we = 0;
 	
 	reg [4:0] r31 = 31;
 	reg [4:0] XP = 30;
@@ -29,26 +29,20 @@ module regfile (
 	// TODO(magendanz) neg? Shouldnt it be pos?
 	// We also need to make sure reads take no clock cycles
 	// and writes complete on posedge.
-	always @(negedge clock) begin
-		ra1 <= ra;
-		data[r31] <= 0; // TODO(magendanz) Just wire this
-		we <= werf;
+
+	assign we = werf;
+	assign wa = (wasel)? XP : rc;	
+
+	always @(posedge clock) begin
 		
-		if (ra2sel) begin
-		  ra2 <= rc;
-		end
-		else ra2 <= rb;
-		
-		if (wasel) begin
-		  wa <= XP;
-		end
-		else wa <= rc;
-		
-		radata <= data[ra1];
-        rbdata <= data[ra2];
-		
-		if (we) begin
+		if (!wasel && we) begin
 		  data[wa] <= wdata;
 		end
 	end
+	
+	assign ra1 = ra;
+	assign ra2 = (ra2sel)? rc : rb;
+	assign radata = data[ra1];
+        assign rbdata = data[ra2];
+	
 endmodule
